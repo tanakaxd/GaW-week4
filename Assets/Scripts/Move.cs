da7f737e12ae0228a7981b0ba0de1c799b2e7467
tanakaXD <ttnk.0112@gmail.com>
2020-04-07 10:26:00 +0900

    initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Move : MonoBehaviour
+{
+    private Camera cam;
+
+    public NavMeshAgent agent;
+
+    //public ThirdPersonCharacter character;
+
+    public Rigidbody rb;
+    private void Awake()
+    {
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("clicked");
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            //Debug.Log(Physics.Raycast(ray, out hit));
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+                //Debug.Log("hit");
+            }
+        }
+
+        //if (agent.remainingDistance > agent.stoppingDistance)
+        //{
+        //    character.Move(agent.desiredVelocity, false, false);
+        //}
+        //else
+        //{
+        //    character.Move(Vector3.zero, false, false);
+        //}
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 forward = transform.forward * speed * Time.deltaTime * vertical;
+        Vector3 right = transform.right * speed * Time.deltaTime * horizontal;
+
+        rb.MovePosition(transform.position + forward + right);
+    }
+}
